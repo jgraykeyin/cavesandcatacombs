91591e37e03ecf4aa737c1de59bd01c97f33b107
jgraykeyin <justin.gray@keyin.com>
2020-10-05 22:37:38 -0230

    Added Move and Look functions

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-print("----------------------")
-print("Welcome to Not Diablo!")
-playername = input("Player Name: ")
+import random
+import os
 
 # Setup the player starts
 ########$ HP,Atk,Lvl,XP
@@ -9,37 +8,55 @@ player = [20,1,1,1]
 # Game settings
 hasMonster = False
 hasLoot = False
-introMessage = True
+roomMsg = ""
+gameStop = 0
 
 # Function to display the currently available player-commands
 def showCommands():
     global command
-    command = input("Available Commands: [M]ove [L]ook >> ")
+    command = input("Available Commands: [Q]uit [L]ove [M]ook >> ")
+
 
 def parseCommand(command):
+    global gameStop
     if command.upper() == "M":
-        pass
-        # Trigger a move function to allow the user to progress
-        # moveNext()
-    if command.upper() == "L":
-        pass
-        # Trigger a look function to inspect the current room
-        # lookRoom()
+        moveNext()
+    elif command.upper() == "L":
+        lookRoom()
+    elif command.upper() == "Q":
+        gameStop = 1
 
+def lookRoom():
+    if hasLoot == False:
+        print("You can't find any loot in this area.\n")
+
+    if hasMonster == False:
+        print("You can't see any monsters in this area.\n")
+
+def moveNext():
+    global roomMsg
+    roomMsg = "You continue into the next room...\n"    
+
+os.system('clear')
+print("----------------------")
+print("Welcome to The Really Dangerous Place!\nFind the amazing secret treasure to become the hero of all the things!\n")
+playername = input("Player Name: ")
 
 # Start a game loop
-while True:
-    if introMessage is True:
-        print("Welcome to The Really Dangerous Place!\nFind the amazing secret treasure to become the hero of all the things!\n")
-        print("{} -- HP:{} Atk:{} Lvl:{}".format(playername,player[0],player[1],player[2]))
-        introMessage=False
-    
+while gameStop == 0:
+
+    if roomMsg:
+        print(roomMsg)
+
+    # Display the player stats
+    print("{} -- HP:{} Atk:{} Lvl:{}".format(playername,player[0],player[1],player[2]))
+
     # Display the available commands.
     showCommands()
+    os.system('clear')
 
     # Check for user input commands
     parseCommand(command)
 
 
-    #Just using this break while testing, remove this to let the game loop run
-    break
+
